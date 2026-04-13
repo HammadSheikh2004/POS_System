79ab0f7015ad4500e7ac5d2e09753808c9e944f9
Hammad Sheikh <hammadsheikh5237@gmail.com>
2026-04-13 07:50:19 +0500

    All things are correct

--- pos_backend/pos_backend/obj/Debug/net10.0/pos_backend.AssemblyInfo.cs
+++ pos_backend/pos_backend/obj/Debug/net10.0/pos_backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f0b11d47d2129e2dacf5d47f3dbe3070c96f5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e283533d07b943cf655a3aefe3ec3656d25745")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
